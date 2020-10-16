d6be670c3997701763e84b9cf09283ff35dcb29a
Rachel Braband <rebraband@gmail.com>
2020-10-16 12:40:40 -0500

    added Course model/migration

---
+++ models/course.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+      Course.belongsTo(User, { 
+        foreignKey: 'userId',
+        as: "author"
+      })
+    }
+  };
+  Course.init({
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    estimatedTime: DataTypes.STRING,
+    materialsNeeded: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: sequelize.User,
+        key: "id",
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Course',
+  });
+  return Course;
+};
