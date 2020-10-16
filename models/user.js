4c89250d0dbe2c3f32ac453c8bd8802bb9e317c9
Rachel Braband <rebraband@gmail.com>
2020-10-16 13:31:03 -0500

    commit

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate({ Course }) {
       User.hasMany(Course, { 
         foreignKey: 'userId',
-        name: "userId"
+        as: "author"
       })
     }
   };
